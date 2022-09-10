99664efd05504ceabb57d660e7fb274f068896bd
sujin0202 <sujinlee@gmail.com>
2022-09-10 09:10:04 +0900

    initial

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('the counter starts at 0', () => {
+  // App Component Rendering
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // screen object를 이용해 원하는 element에 접근 (접근 시 ID로 접근)
+  const counterElement = screen.getByTestId('counter');
+  // id가 counter인 element의 text가 0인지 테스트
+  expect(counterElement).toHaveTextContent(0);
 });
+
+test('minus button has correct text', () => {
+  render(<App />)
+  const buttonElement = screen.getByTestId('minus-button')
+  expect(buttonElement).toHaveTextContent('-')
+});
+
+test('plus button has correct text', () => {
+  render(<App />)
+  const buttonElement = screen.getByTestId('plus-button')
+  expect(buttonElement).toHaveTextContent('+')
+});
+
+test('When the + button is pressed, the counter changes to 1', () => {
+  render(<App />)
+  const buttonElement = screen.getByTestId('plus-button')
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId('counter')
+  expect(counterElement).toHaveTextContent(1)
+})
+
+test('on/off button has blue color', () => {
+  render(<App />)
+  const buttonElement = screen.getByTestId('on-off-button')
+  expect(buttonElement).toHaveStyle({ backgroundColor: 'blue' })
+})
+
+test('on/off button is pressed, the button is disabled', () => {
+  render(<App />)
+  const onoffButtonElement = screen.getByTestId('on-off-button')
+  fireEvent.click(onoffButtonElement);
+  const plusButtonElement = screen.getByTestId('plus-button')
+  expect(plusButtonElement).toBeDisabled();
+})
